de7686a5e6824f168bed14dbe69ebd9f2f886cf3
a.burgart02 <a.burgart02@gmail.com>
2023-12-19 03:20:29 +0500

    Return imports for build

--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 from app.application_window import ApplicationWindow
+from app.messages import message_pb2
+from application_window import ApplicationWindow
+import ctypes
+import matplotlib
+import google.protobuf
+import google.protobuf.descriptor
+import serial_asyncio
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
