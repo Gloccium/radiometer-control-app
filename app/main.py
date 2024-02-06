341e4628a8792a98f6d3d1234434ce0ee9ccf130
a.burgart02 <a.burgart02@gmail.com>
2024-02-06 06:28:23 +0500

    Add imports for build

--- app/main.py
+++ app/main.py
@@ -1,13 +1,27 @@
 import sys
 import asyncio
 import qasync
+import os
+import base64
+import jsonschema
+import queue
+import binascii
+import time
+import aiohttp
+import datetime
+import json
 import ctypes
 import matplotlib
 import google.protobuf
 import google.protobuf.descriptor
 import serial_asyncio
-from PyQt5.QtWidgets import QApplication
+import serial.tools
+import serial.tools.list_ports
+import scipy
+import app
 
+from scipy import signal
+from PyQt5.QtWidgets import QApplication
 from app.messages import message_pb2
 from app.windows.application_window import ApplicationWindow
 
