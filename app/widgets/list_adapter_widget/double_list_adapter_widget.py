d20f4da6723f8827ee9e4e90fa80af5e8430714a
a.burgart02 <a.burgart02@gmail.com>
2024-02-06 18:17:50 +0500

    Add label stylesheet

--- app/widgets/list_adapter_widget/double_list_adapter_widget.py
+++ app/widgets/list_adapter_widget/double_list_adapter_widget.py
@@ -8,12 +8,14 @@ class DoubleListAdapter(QWidget):
         self.vBox = QVBoxLayout()
         self.name = QLabel()
         self.name_label = QLabel(name)
+        self.name_label.setStyleSheet('QLabel { color: grey; }')
         self.vBox.addWidget(self.name_label)
         self.vBox.addWidget(self.name)
 
         self.vBoxSecond = QVBoxLayout()
         self.description = QLabel()
         self.description_label = QLabel(description)
+        self.description_label.setStyleSheet('QLabel { color: grey; }')
         self.vBoxSecond.addWidget(self.description_label)
         self.vBoxSecond.addWidget(self.description)
 
