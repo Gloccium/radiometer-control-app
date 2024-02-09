776119982e66f27bb8b1ebfc5ca2de70c22bddc2
a.burgart02 <a.burgart02@gmail.com>
2024-02-09 05:18:20 +0500

    Add configuration file

--- app/windows/settings_window.py
+++ app/windows/settings_window.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 from PyQt5.QtWidgets import QWidget, QLineEdit, QPushButton, QFileDialog, QMessageBox
@@ -8,8 +9,8 @@ from app.utils.error_messages import show_error
 class SettingsWindow(QWidget):
     def __init__(self):
         super().__init__()
-        self.records_directory_path = './records'
-        self.calibration_directory_path = './calibrationData'
+        self.records_directory_path = ''
+        self.calibration_directory_path = ''
         self.server_address = 'localhost:7209'
         self.server_address_input = QLineEdit(self)
         self.records_directory = QLineEdit(self)
@@ -17,7 +18,9 @@ class SettingsWindow(QWidget):
         self.calibration_directory = QLineEdit(self)
         self.calibration_directory_button = QPushButton('Выбрать директорию для калибровок', self)
         self.save_button = QPushButton('Сохранить', self)
+        self.init_config_file()
         self.init_default_directories()
+        self.load_configuration()
         self.configure_elements()
 
     def select_records_directory(self):
@@ -52,6 +55,34 @@ class SettingsWindow(QWidget):
         except Exception as e:
             print(e)
             show_error(QMessageBox.Critical, "Ошибка при создании директории калибровок", "Директория не может быть создана")
+
+        self.save_config_file()
+
+    def save_config_file(self):
+        config_data = {
+            "records_directory_path": self.records_directory_path,
+            "calibration_directory_path": self.calibration_directory_path,
+        }
+        json_config_data = json.dumps(config_data)
+        with open('config.json', 'w') as f:
+            f.write(json_config_data)
+
+    def load_configuration(self):
+        with open('config.json', 'r') as f:
+            config_data = json.loads(f.read())
+            self.records_directory_path = config_data["records_directory_path"]
+            self.calibration_directory_path = config_data["calibration_directory_path"]
+
+    @staticmethod
+    def init_config_file():
+        config_data = {
+            "records_directory_path": './records',
+            "calibration_directory_path": './calibrationData',
+        }
+        json_config_data = json.dumps(config_data)
+        if not os.path.exists('config.json'):
+            with open('config.json', 'w') as f:
+                f.write(json_config_data)
 
     @staticmethod
     def init_default_directories():
